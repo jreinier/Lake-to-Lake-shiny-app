ea703d501270e66bdaefb48597b57e7c6294af8a
jreinier <jreinier@users.noreply.github.com>
2016-11-30 15:24:52 -0500

    Update server.R

--- server.R
+++ server.R
@@ -7,12 +7,21 @@ library(agricolae)
 library(quantmod)
 library(dplyr)
 library(shinyTime)
+library(grid)
 
 drv <- dbDriver("PostgreSQL")
 conn <- dbConnect(drv, host="10.0.0.27", port="5432",
                   dbname="NR_monitoring", user="jreinier", password="carex91308")
 
 hydrodata <- dbGetQuery(conn, "SELECT date, timestamp, reserv, level_cm, serial FROM nr_misc.water_level_data WHERE reserv = 'big creek' AND level_raw < 9.15;")
+
+usgs.sheldon <- dbGetQuery(conn, "SELECT datetime, par_value FROM nr_misc.usgs_abram_creek_discharge WHERE site_no = '04201515';")
+
+usgs.kolthoff <- dbGetQuery(conn, "SELECT datetime, par_value FROM nr_misc.usgs_abram_creek_discharge WHERE site_no = '04201526';")
+
+cle.precip <- dbGetQuery(conn, "SELECT datetime, hpcp FROM nr_misc.cle_noaa_precip_08_to_14;")
+
+
 
 
 server <- function(input, output) {
@@ -24,15 +33,57 @@ server <- function(input, output) {
              serial == input$serial)
   })
   
+  sheldon <- reactive({ 
+    usgs.sheldon %>%
+      filter(datetime >= input$timestamp[1],
+             datetime <= input$timestamp[2]
+)
+  })
+  
+  kolthoff <- reactive({ 
+    usgs.kolthoff %>%
+      filter(datetime >= input$timestamp[1],
+             datetime <= input$timestamp[2]
+      )
+  })
+  
+  precip <- reactive({ 
+    cle.precip %>%
+      filter(datetime >= input$timestamp[1],
+             datetime <= input$timestamp[2]
+      )
+  })
+  
   output$plot <- renderPlot({
     
-    p <- ggplot(newdata(), aes(x = timestamp, y = level_cm)) +
+    p1 <- ggplot(newdata(), aes(x = timestamp, y = level_cm)) +
       geom_line() +
       xlab("Date") +
       ylab("Water Level (cm)") +
       ggtitle("Test Hydrograph")
     
-    print(p)
+    p2 <- ggplot(sheldon(), aes(x = datetime, y = par_value)) +
+      geom_line() +
+      xlab("Date") +
+      ylab("Discharge Rate") +
+      ggtitle("USGS-Sheldon Road")
+    
+    p3 <- ggplot(kolthoff(), aes(x = datetime, y = par_value)) +
+      geom_line() +
+      xlab("Date") +
+      ylab("Discharge Rate") +
+      ggtitle("USGS-Kolthoff Road")
+    
+    p4 <- ggplot(precip(), aes(x = datetime, y = hpcp)) +
+      geom_line() +
+      xlab("Date") +
+      ylab("Precipitation") +
+      ggtitle("NOAA Precip (cm)")
+    
+    grid.newpage()
+    plot.all <- grid.draw(rbind(ggplotGrob(p1), ggplotGrob(p2), ggplotGrob(p3), ggplotGrob(p4), size = "last"))
+    
+    print(plot.all)
     })
   
 }
